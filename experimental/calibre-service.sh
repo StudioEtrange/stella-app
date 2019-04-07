ad2397dabd8ad3b602525221a7e43d42d227780a
StudioEtrange <sboucault@gmail.com>
2019-04-07 17:47:10 +0200

    Update calibre-service.sh

--- experimental/calibre-service.sh
+++ experimental/calibre-service.sh
@@ -8,12 +8,17 @@ STELLA_APP_PROPERTIES_FILENAME="calibre-service.properties"
 # TODO WIP - NOT FINISHED
 
 # TODO
-# objective : have Calibre content server AND Calibre-web (https://github.com/janeczku/calibre-web/) interface to manage and get ebook AND lazy librarian to get metadata
-#             we dont need calibre gui access
+# objective : 
+#             1/have Calibre content server 
+#             2/AND Calibre-web (https://github.com/janeczku/calibre-web/) interface to manage and get ebook 
+#             3/AND lazy librarian to get metadata
+#             
+# NOTE : we dont need calibre gui access
 
-# To get calibre content server AND lazylibrarian WORK on https://github.com/StudioEtrange/docker-lazylibrarian-calibre and get specific version of calibre (https://github.com/kovidgoyal/calibre/tags)
-#       alternative For calibre content server : use https://github.com/aptalca/docker-rdp-calibre (but we do not need rdp over calibre gui)
-# To get calibre web WORK on  https://github.com/Technosoft2000/docker-calibre-web OR https://github.com/linuxserver/docker-calibre-web
-#       alternative for calibre web https://github.com/janeczku/calibre-web/
+# To get calibre content server (1) AND lazylibrarian (3) WORK on https://github.com/StudioEtrange/docker-lazylibrarian-calibre and get specific version of calibre (https://github.com/kovidgoyal/calibre/tags)
+#       alternative For calibre content server (1) : use https://github.com/aptalca/docker-rdp-calibre (but we do not need rdp over calibre gui)
+
+# To get calibre web (2) WORK on  https://github.com/Technosoft2000/docker-calibre-web OR https://github.com/linuxserver/docker-calibre-web
+#       alternative for calibre web (2) : https://github.com/janeczku/calibre-web/
 
 # optional : unlock ebook tools : https://github.com/apprenticeharper/DeDRM_tools
