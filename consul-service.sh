1206bc4133d0796fbb06e241222cf8a286a670d1
StudioEtrange <sboucault@gmail.com>
2019-09-26 04:32:15 +0200

    consul service : add mode for macos desktop

--- consul-service.sh
+++ consul-service.sh
@@ -15,21 +15,29 @@ STELLA_APP_PROPERTIES_FILENAME="consul-service.properties"
 # docker logs consul-service-server-1
 # see http://$(docker-machine ip $DOCKER_MACHINE_NAME):8500/ui
 
+# Example with macos docker desktop
+# ./consul-service.sh create server 1 -m
+# ./consul-service.sh create client 2 -m --joinid=1
+# ./consul-service.sh list members 1
+
+
 # NOTE :
-# Consul should always be run with --net=host in Docker because Consul's consensus and gossip protocols are sensitive to delays and packet loss,
+# Consul should be run with --net=host in Docker because Consul's consensus and gossip protocols are sensitive to delays and packet loss,
 # so the extra layers involved with other networking types are usually undesirable and unnecessary.
 
-# NOTE : docker consul -bind and -client options can not be 0.0.0.0
-# CONSUL_CLIENT_INTERFACE and/or CONSUL_BIND_INTERFACE could be use to specify interface
+# NOTE : 
+#    - CONSUL_CLIENT_INTERFACE and/or CONSUL_BIND_INTERFACE could be use to specify interface
+#    - "disable_update_check": true ==> disable HTTP request to hashicorp to check critical update
+
 
 # SERVICE INFO --------------------------------------
 DEFAULT_HTTP_PORT=8500
 DEFAULT_DNS_PORT=8600
 
-DEFAULT_IP=$STELLA_HOST_DEFAULT_IP
 DEFAULT_SERVICE_NAME="consul-service"
 
 # DOCKER IMAGES INFO --------------------------------------
+# https://github.com/hashicorp/docker-consul
 DEFAULT_DOCKER_IMAGE="consul"
 DEFAULT_DOCKER_IMAGE_VERSION="1.0.6"
 
@@ -43,41 +51,46 @@ function usage() {
   echo "NOTE : it cand provide a server consul agent or a client only consul agent"
   echo "----------------"
   echo "o-- command :"
-  echo "L     create <client|server> <id> [--version=<version>] [--http=<port>] [--dns=<port>] [--ip=<ip>|--if=<interface>] [--datacenter=<string>] [--domainname=<string>] [--consulip=<ip>] [-- additional docker run options] : create & launch service (must be use once before starting/stopping service)"
+  echo "L     create <server> <id> [--version=<version>] [--http=<port>] [--dns=<port>] [--ip=<ip>|--if=<interface>] [--datacenter=<string>] [--domainname=<string>] [-- additional docker run options] : create & launch service (must be use once before starting/stopping service)"
+  echo "L     create <client> <id> [--version=<version>] [--joinip=<ip>|joinid=<id>]  [--http=<port>] [--dns=<port>] [--ip=<ip>|--if=<interface>] [--datacenter=<string>] [--domainname=<string>] [-- additional docker run options] : create & launch service (must be use once before starting/stopping service)"
   echo "L     start <client|server> <id> : start service"
   echo "L     stop <client|server> <id> : stop service"
   echo "L     status <client|server> <id> : give service status info"
   echo "L     shell <client|server> <id> : launch a shell inside running service"
   echo "L     destroy <client|server> <id> [--version=<version>] : destroy service"
+  echo "L     list members [<id>] : will list consul cluster members, by connecting to an agent <id> (1 by default)"
   echo "o-- options :"
   echo "L     --http : consul http api port"
   echo "L     --dns : consul dns port"
   echo "L     --version : consul image version"
-  echo "L     --ip : ip on which all consul agent services will listen. Cannot be 0.0.0.0 (Use --if or --ip. --if have priority)"
+  echo "L     --ip : ip on which all consul agent services will listen. (Use --if or --ip. --if have priority)"
   echo "L     --if : interface on which all consul agent services will listen (Use --if or --ip, --if have priority)"
-  echo "L     --consulip : ip of a consul server agent which the client will join (usefull only with <client>)"
+  echo "L     --joinip : ip of a consul server agent which the client will join (usefull only with <client>)"
   echo "L     --debug : active some debug trace"
   echo "L     --datacenter : consul datacenter name"
   echo "L     --domain : consul domain name"
+  echo "L     --macdesk : Active a specific behaviour for macos docker desktop support. Will ignore any ip, if options. Will keep HTTP and DNS port options for server but ignore them for client."
 
 }
 
 # COMMAND LINE -----------------------------------------------------------------------------------
 PARAMETERS="
-ACTION=											'' 			a				'create start stop status shell destroy' '1'
-TARGET=											'' 			a				'client server' '1'
-ID=											'' 			s				'' '1'
+ACTION=											'' 			a				'create start stop status shell destroy list' '1'
+TARGET=											'' 			a				'client server members' '1'
+ID=											'' 			s				'' '0'
 "
 OPTIONS="
 HTTP='$DEFAULT_HTTP_PORT' 						'' 			'string'				s 			0			''		  Consul http api port.
 DNS='$DEFAULT_DNS_PORT' 						'' 			'string'				s 			0			''		  Consul dns port.
-IP='$DEFAULT_IP' 						'' 			'string'				s 			0			''		  IP on which all consul agent services will listen. Use --if or --ip, if both used --ip is used.
+IP='' 						'' 			'string'				s 			0			''		  IP on which all consul agent services will listen. Use --if or --ip, if both used --ip is used.
 IF='' 						'' 			'string'				s 			0			''		  Interface on which all consul agent services will listen. Use --if or --ip, if both used --ip is used.
-CONSULIP='$DEFAULT_IP' 						'' 			'string'				s 			0			''		  IP of a consul server agent which the client will join.
+JOINIP='' 						'' 			'string'				s 			0			''		  IP of a consul server agent which the client will join.
+JOINID='' 						'' 			'string'				s 			0			''		  ID of a consul server agent which the client will join.
 VERSION='$DEFAULT_DOCKER_IMAGE_VERSION' 			'v' 			'string'				s 			0			''		  Consul image version.
 DEBUG=''            'd'    		''            		b     		0     		'1'           			Active some debug trace.
 DATACENTER='' 			'' 			'string'				s 			0			''		  Consul datacenter name.
 DOMAINNAME='' 			'' 			'string'				s 			0			''		  Consul domain name.
+MACDESK=''            'm'    		''            		b     		0     		'1'           			Special tag to active a specific behaviour for macos docker desktop support. Will ignore any ip, if options. Will keep HTTP and DNS port options for server but ignore them for client.
 "
 $STELLA_API argparse "$0" "$OPTIONS" "$PARAMETERS" "$STELLA_APP_NAME" "$(usage)" "EXTRA_ARG DOCKERARG" "$@"
 
@@ -100,22 +113,32 @@ __get_ip_from_hostname() {
 }
 
 # ------------- COMPUTE ARGUMENTS AND VALUES -------------------------
+[ "$ID" = "" ] && ID="1"
 DOCKER_IMAGE_VERSION=$VERSION
 DOCKER_URI=$DEFAULT_DOCKER_IMAGE
 [ ! -z "$DOCKER_IMAGE_VERSION" ] && DOCKER_URI=$DOCKER_URI:$DOCKER_IMAGE_VERSION
 SERVICE_NAME=$DEFAULT_SERVICE_NAME
-SERVICE_NAME=${SERVICE_NAME}-${TARGET}-${ID}
+SERVICE_NAME=${SERVICE_NAME}-${ID}
 
 
 
-# convert hostname to IP
-CONSULIP="$(__get_ip_from_hostname $CONSULIP)"
 
 # test docker client is installed in this system
 $STELLA_API require "docker" "docker" "SYSTEM"
 
 
 # ------------- ACTIONS -------------------------
+
+if [ "$ACTION" = "list" ]; then
+  case $TARGET in
+    members )
+       __log_run docker exec -t $SERVICE_NAME consul members
+    ;;
+  esac
+fi
+
+
+
 if [ "$ACTION" = "create" ]; then
     # delete and stop previously stored container and volume
     __log_run docker stop $SERVICE_NAME 2>/dev/null
@@ -128,58 +151,127 @@ if [ "$ACTION" = "create" ]; then
         [ ! "$DATACENTER" = "" ]  && _OPT="$_OPT -datacenter=$DATACENTER"
         [ ! "$DOMAINNAME" = "" ]  && _OPT="$_OPT -domain=$DOMAINNAME"
 
-        # NOTE : , "disable_update_check": true ==> disable HTTP request to hashicorp to check critical update
-        if [ "$IF" = "" ]; then
-
-          CONSUL_AGENT_BIND_IP="$(__get_ip_from_hostname $IP)"
-
+        # for macos docker desktop
+        # https://stackoverflow.com/questions/41228968/accessing-consul-ui-running-in-docker-on-osx
+        # NOTE : template for infer adress https://github.com/hashicorp/go-sockaddr/tree/master/cmd/sockaddr    
+        if [ "$MACDESK" = "1" ]; then
           __log_run docker run -d \
-              --name $SERVICE_NAME \
-              --restart always \
-              --net=host \
-              -v $SERVICE_NAME:/consul/data \
-              -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
-              $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
-              -server -bootstrap-expect=1 -ui \
-              -bind=$CONSUL_AGENT_BIND_IP -client=$CONSUL_AGENT_BIND_IP $_OPT
+            --name $SERVICE_NAME \
+            -p $HTTP:8500 -p $DNS:8600 -P \
+            --restart always \
+            -v $SERVICE_NAME:/consul/data \
+            -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
+            $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=8500 -dns-port=8600 \
+            -server -bootstrap-expect=1 -ui -bind='{{ GetPrivateIP }}' -client=0.0.0.0
+
         else
-           __log_run docker run -d \
-              --name $SERVICE_NAME \
-              --restart always \
-              --net=host \
-              -v $SERVICE_NAME:/consul/data \
-              -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
-              -e CONSUL_BIND_INTERFACE=$IF -e CONSUL_CLIENT_INTERFACE=$IF \
-              $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
-              -server -bootstrap-expect=1 -ui \
-              $_OPT
+
+          
+          if [ ! "$IP" = "" ]; then
+
+            CONSUL_AGENT_BIND_IP="$(__get_ip_from_hostname $IP)"
+
+            __log_run docker run -d \
+                --name $SERVICE_NAME \
+                --restart always \
+                --net=host \
+                -v $SERVICE_NAME:/consul/data \
+                -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
+                $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+                -server -bootstrap-expect=1 -ui \
+                -bind=$CONSUL_AGENT_BIND_IP -client=$CONSUL_AGENT_BIND_IP \
+                $_OPT
+          else
+            if [ ! "$IF" = "" ]; then
+              __log_run docker run -d \
+                  --name $SERVICE_NAME \
+                  --restart always \
+                  --net=host \
+                  -v $SERVICE_NAME:/consul/data \
+                  -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
+                  -e CONSUL_BIND_INTERFACE=$IF -e CONSUL_CLIENT_INTERFACE=$IF \
+                  $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+                  -server -bootstrap-expect=1 -ui \
+                  $_OPT
+              else
+                __log_run docker run -d \
+                  --name $SERVICE_NAME \
+                  --restart always \
+                  --net=host \
+                  -v $SERVICE_NAME:/consul/data \
+                  -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
+                  $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+                  -server -bootstrap-expect=1 -ui \
+                  $_OPT
+
+              fi
+          fi
         fi
         ;;
 
+
+
+
       client )
-        if [ "$IF" = "" ]; then
-          CONSUL_AGENT_BIND_IP="$(__get_ip_from_hostname $IP)"
+
+        # convert hostname to IP
+        [ ! "$JOINIP" = "" ] && JOINIP="$(__get_ip_from_hostname $JOINIP)"
+        if [ ! "$JOINID" = "" ]; then
+              JOINIP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' ${DEFAULT_SERVICE_NAME}-${JOINID})"
+        fi
+
+        if [ "$JOINIP" = "" ]; then
+          echo "** ERROR : precise a consul IP to join with --joinip or an instance id with --joinid"
+          exit 1
+        fi
+
+        if [ "$MACDESK" = "1" ]; then
 
           __log_run docker run -d \
             --name $SERVICE_NAME \
+            -P \
             --restart always \
-            --net=host \
             -v $SERVICE_NAME:/consul/data \
-            -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true, "disable_update_check": true}' \
-            $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
-            -retry-join=$CONSULIP -bind=$CONSUL_AGENT_BIND_IP -client=$CONSUL_AGENT_BIND_IP
+            -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
+            $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=8500 -dns-port=8600 \
+            -retry-join=$JOINIP -bind='{{ GetPrivateIP }}' -client=0.0.0.0
+
         else
+        
 
-          __log_run docker run -d \
-            --name $SERVICE_NAME \
-            --restart always \
-            --net=host \
-            -v $SERVICE_NAME:/consul/data \
-            -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true, "disable_update_check": true}' \
-            -e CONSUL_BIND_INTERFACE=$IF -e CONSUL_CLIENT_INTERFACE=$IF \
-            $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
-            -retry-join=$CONSULIP
+          if [ ! "$IP" = "" ]; then
+            CONSUL_AGENT_BIND_IP="$(__get_ip_from_hostname $IP)"
 
+            __log_run docker run -d \
+              --name $SERVICE_NAME \
+              --restart always \
+              --net=host \
+              -v $SERVICE_NAME:/consul/data \
+              -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true, "disable_update_check": true}' \
+              $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+              -retry-join=$JOINIP -bind=$CONSUL_AGENT_BIND_IP -client=$CONSUL_AGENT_BIND_IP
+          else
+            if [ ! "$IF" = "" ]; then
+              __log_run docker run -d \
+                --name $SERVICE_NAME \
+                --restart always \
+                --net=host \
+                -v $SERVICE_NAME:/consul/data \
+                -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true, "disable_update_check": true}' \
+                -e CONSUL_BIND_INTERFACE=$IF -e CONSUL_CLIENT_INTERFACE=$IF \
+                $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+                -retry-join=$JOINIP
+            else
+              __log_run docker run -d \
+                --name $SERVICE_NAME \
+                --restart always \
+                --net=host \
+                -v $SERVICE_NAME:/consul/data \
+                -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true, "disable_update_check": true}' \
+                $DOCKERARG $DOCKER_URI agent -node=$SERVICE_NAME -http-port=$HTTP -dns-port=$DNS \
+                -retry-join=$JOINIP
+            fi
+          fi
         fi
         ;;
 
